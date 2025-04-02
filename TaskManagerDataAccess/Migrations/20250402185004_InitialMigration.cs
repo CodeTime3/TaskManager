ddc46ac792bc69b81e434d7644d40f50969647dd
CodeTime3 <var.codetime@gmail.com>
2025-04-02 20:58:50 +0200

    added migration and create db

---
+++ TaskManagerDataAccess/Migrations/20250402185004_InitialMigration.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TaskManagerDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ProjectName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    ProjectStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.ProjectId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    UserMail = table.Column<string>(type: "varchar(255)", nullable: false),
+                    UserHash = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "project_tasks",
+                columns: table => new
+                {
+                    ProjectTaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    TaskTitle = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    TaskText = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    TaskStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_project_tasks", x => x.ProjectTaskId);
+                    table.ForeignKey(
+                        name: "FK_project_tasks_projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "user_invites",
+                columns: table => new
+                {
+                    InviteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    InviteMailObject = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    InviteMailText = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    InviteLink = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_invites", x => x.InviteId);
+                    table.ForeignKey(
+                        name: "FK_user_invites_projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_invites_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "user_projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_projects", x => new { x.UserId, x.ProjectId });
+                    table.ForeignKey(
+                        name: "FK_user_projects_projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_projects_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "notes",
+                columns: table => new
+                {
+                    NotedId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ProjectTaskId = table.Column<int>(type: "int", nullable: false),
+                    NoteTitle = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    NoteText = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notes", x => x.NotedId);
+                    table.ForeignKey(
+                        name: "FK_notes_project_tasks_ProjectTaskId",
+                        column: x => x.ProjectTaskId,
+                        principalTable: "project_tasks",
+                        principalColumn: "ProjectTaskId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_notes_ProjectTaskId",
+                table: "notes",
+                column: "ProjectTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_project_tasks_ProjectId",
+                table: "project_tasks",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_invites_ProjectId",
+                table: "user_invites",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_invites_UserId",
+                table: "user_invites",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_projects_ProjectId",
+                table: "user_projects",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_UserMail",
+                table: "users",
+                column: "UserMail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_UserName",
+                table: "users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notes");
+
+            migrationBuilder.DropTable(
+                name: "user_invites");
+
+            migrationBuilder.DropTable(
+                name: "user_projects");
+
+            migrationBuilder.DropTable(
+                name: "project_tasks");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "projects");
+        }
+    }
+}
