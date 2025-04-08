bef4968c076e77458ac10a8649e528bb1166a720
CodeTime3 <var.codetime@gmail.com>
2025-04-08 09:20:36 +0200

    made crud for user enity

---
+++ TaskManagerDataAccess/DataAccess/Services/UserService.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerDataAccess.Models;
+
+namespace TaskManagerDataAccess.DataAccess.Services;
+
+public class UserService
+{
+    private readonly TaskManagerDbContext _dbContext;
+
+    public UserService (TaskManagerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> AddUser (User user)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+
+        _dbContext.Add(user);
+        await _dbContext.SaveChangesAsync();
+
+        return user;
+    }
+
+    public async Task<User[]> GetAllUsers ()
+    {
+        var users = await _dbContext.Users
+            .FromSql($"select * from users")//change users with a parameter
+            .ToListAsync();
+
+        ArgumentNullException.ThrowIfNull(users);
+        
+        return users.ToArray();
+    }
+
+    public async Task<User> GetUsers (int id)
+    {
+        var user = await _dbContext.Users.FindAsync(id);
+
+        ArgumentNullException.ThrowIfNull(user);
+
+        return user;
+    }
+
+    public async Task<User> UpdateUser (User user)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+
+        _dbContext.Update(user);
+        await _dbContext.SaveChangesAsync();
+
+        return user;
+    }
+
+    public async Task DeleteUser (int id)
+    {
+        var user = _dbContext.Users.Find(id);
+
+        ArgumentNullException.ThrowIfNull(user);
+
+        _dbContext.Remove(user);
+        await _dbContext.SaveChangesAsync();
+    }
+}
