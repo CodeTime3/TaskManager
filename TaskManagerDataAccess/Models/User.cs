956138d67731b47b0ea23345e889451ca3b8d796
CodeTime3 <var.codetime@gmail.com>
2025-04-08 09:19:16 +0200

    Added 2 constructors

--- TaskManagerDataAccess/Models/User.cs
+++ TaskManagerDataAccess/Models/User.cs
@@ -20,4 +20,13 @@ public class User
     [MaxLength(50)]
     [MinLength(8)]
     public string UserHash { get; set; }
+
+    public User(string userName, string userMail, string userHash)
+    {
+        UserName = userName;
+        UserMail = userMail;
+        UserHash = userHash;
+    }
+
+    public User() {}
 }
