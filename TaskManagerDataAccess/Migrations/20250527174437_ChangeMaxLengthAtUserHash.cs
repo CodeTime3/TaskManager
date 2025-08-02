3161ea1f34b9756de14ed8e4f7ce90974a91d4ee
CodeTime3 <var.codetime@gmail.com>
2025-08-02 12:20:42 +0200

    changed max lenght at userhash

---
+++ TaskManagerDataAccess/Migrations/20250527174437_ChangeMaxLengthAtUserHash.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagerDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMaxLengthAtUserHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserHash",
+                table: "users",
+                type: "varchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserHash",
+                table: "users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
